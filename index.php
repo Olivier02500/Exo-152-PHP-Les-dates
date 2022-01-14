d3b67346d701fe5039c992f253ca1b1c4359acc8
Olivier02500 <olivier.peuchet@hotmail.com>
2022-01-14 11:13:40 +0100

    make exo 152

--- index.php
+++ index.php
@@ -1,9 +1,20 @@
 <?php
 
 // Premiere ligne
+$firstDate = date("D M J Y");
+echo $firstDate;
 
+echo "<br><br>";
 
+//deuxiéme ligne
+$secondDate = strtotime("2018.12.10");
+echo date('Y.m.d', $secondDate);
 
+echo "<br><br>";
+
+//troisiéme ligne
+$thirdDate = strtotime("11:35:07");
+echo date('H:i:s', $thirdDate);
 
 
 
